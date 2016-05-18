a9bbcc38dbc377c70d5d106d5779c2d451c55ffc
doktordirk <doktordirk@outlook.de>
2016-05-18 18:38:26 +0200

    tt

--- dist/system/aurelia-logging-console.js
+++ dist/system/aurelia-logging-console.js
@@ -1,7 +1,9 @@
 'use strict';
 
 System.register(['aurelia-pal', 'aurelia-logging'], function (_export, _context) {
-  var PLATFORM, Logger, _typeof, ConsoleAppender;
+  "use strict";
+
+  var PLATFORM, Logger, ConsoleAppender;
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -16,35 +18,6 @@ System.register(['aurelia-pal', 'aurelia-logging'], function (_export, _context)
       Logger = _aureliaLogging.Logger;
     }],
     execute: function () {
-      _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
-        return typeof obj;
-      } : function (obj) {
-        return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj;
-      };
-
-
-      (function (global) {
-        global.console = global.console || {};
-        var con = global.console;
-        var prop = void 0;
-        var method = void 0;
-        var empty = {};
-        var dummy = function dummy() {};
-        var properties = 'memory'.split(',');
-        var methods = ('assert,clear,count,debug,dir,dirxml,error,exception,group,' + 'groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd,' + 'show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn').split(',');
-        while (prop = properties.pop()) {
-          if (!con[prop]) con[prop] = empty;
-        }while (method = methods.pop()) {
-          if (!con[method]) con[method] = dummy;
-        }
-      })(PLATFORM.global);
-
-      if (PLATFORM.global.console && _typeof(console.log) === 'object') {
-        ['log', 'info', 'warn', 'error', 'assert', 'dir', 'clear', 'profile', 'profileEnd'].forEach(function (method) {
-          console[method] = this.bind(console[method], console);
-        }, Function.prototype.call);
-      }
-
       _export('ConsoleAppender', ConsoleAppender = function () {
         function ConsoleAppender() {
           _classCallCheck(this, ConsoleAppender);
